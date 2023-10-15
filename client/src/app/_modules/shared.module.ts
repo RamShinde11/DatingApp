046a8b25137bd0b341264cff391e5ccc0cab16a1
Ram Shinde <rrpatil.06@gmail.com>
2023-10-15 19:28:02 +0530

    End of Section 8

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 @NgModule({
   declarations: [],
@@ -13,13 +13,14 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
     TabsModule.forRoot(),
     ToastrModule.forRoot({
      positionClass:'toast-bottom-right'
-
-    })
+    }),
+    NgxGalleryModule
   ],
   exports:[
     BsDropdownModule,
     ToastrModule,
-    TabsModule
+    TabsModule,
+    NgxGalleryModule
   ]
 })
 export class SharedModule { }
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,45 +1,70 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
+import {NgxGalleryModule, NgxGalleryOptions} from '@kolkov/ngx-gallery';
+import {NgxGalleryImage} from '@kolkov/ngx-gallery';
+import {NgxGalleryAnimation} from '@kolkov/ngx-gallery';
+import { GalleryModule } from 'ng-gallery';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
+
+
+
 @Component({
   selector: 'app-member-detail',
-  standalone: true,
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.css'],
-  imports:[CommonModule,TabsModule,GalleryModule]
+  imports :[CommonModule,TabsModule,NgxGalleryModule],
+  standalone :true
 })
 export class MemberDetailComponent implements OnInit {
    member: Member | undefined;
-   images: GalleryItem[] = [];
+   galleryOptions: NgxGalleryOptions[] = [];
+   galleryImages: NgxGalleryImage[] = [];
+   
 
    constructor(private memberService: MembersService, private route: ActivatedRoute) { }
 
    ngOnInit(): void {
      this.loadMember();
+
+     this.galleryOptions = [
+       {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+       }
+     ]
+    
    }
 
-   loadMember(){
+   getImages() {
+    if(!this.member) return [];
+    const imageUrls = [];
+    for (const photo of this.member.photos) {
+      imageUrls.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url
+      })
+    }
+    return imageUrls;
+   }
+
+   loadMember() {
     var username =this.route.snapshot.paramMap.get('username');
-    if(!username)return;
+    if(!username) return;
     this.memberService.getMember(username).subscribe({
       next: member =>{ 
-        this.member= member,
-        this.getImages()
+        this.member= member;
+        this.galleryImages= this.getImages();
       }
     })
    }
 
-
-   getImages(){
-    if(!this.member) return;
-    for (const photo of this.member?.photos){
-      this.images.push(new ImageItem({src:photo.url,thumb:photo.url}));
-
-    }
-   }
 }
